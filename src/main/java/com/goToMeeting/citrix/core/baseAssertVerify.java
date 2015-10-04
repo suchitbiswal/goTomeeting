d77df73b4bd2627222fbcf3fb5a269af87812cb7
suchitbiswal <suchitbiswal@10.0.0.121>
2015-10-04 13:17:30 -0700

    Fixed the Defect relatd to verify and assert

--- src/main/java/com/goToMeeting/citrix/core/baseAssertVerify.java
+++ src/main/java/com/goToMeeting/citrix/core/baseAssertVerify.java
@@ -26,7 +26,7 @@ public abstract class baseAssertVerify {
 		return verificationFailures == null ? new ArrayList() : verificationFailures;
 	}
 	
-	public static void verifyEquals(String actual, String expected) throws RuntimeException  {
+	public static void assertEquals(String actual, String expected) throws RuntimeException  {
 		try {
 			Assert.assertEquals(actual, expected);
 			log("Expected value: " + expected + " Actual value: " + actual + " - PASSED");
@@ -37,7 +37,7 @@ public abstract class baseAssertVerify {
 	}
 
 	
-	public static void verifyEquals1(Object actual, Object expected) {
+	public static void verifyEquals(Object actual, Object expected) {
 		try {
 			Assert.assertEquals(actual, expected);
 			log("Expected value: " + expected.toString() + " Actual value: " + actual.toString() + " - PASSED");
